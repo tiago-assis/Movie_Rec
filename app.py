699e4b6217bf20e72e189934f18a0bbd79b38bed
tiago-assis <tiago.miguel.assis@gmail.com>
2024-08-27 02:11:01 +0100

    Changed how psoters are generated so they are clickable now; dialog popup with movie details and recommendations added; recommendation logic added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from st_click_detector import click_detector
 import pandas as pd
 import numpy as np
 from scipy.sparse import load_npz
@@ -6,6 +7,7 @@ import pickle
 import os
 import math
 import time
+import ast
 
 st.set_page_config(layout="wide", page_icon="🎞",
                    page_title="Movie Recommendation")
@@ -22,10 +24,13 @@ def get_data():
         cast_cols = pickle.load(f)
     with open("cleaned_data/production_companies_cols_top500.pickle", "rb") as f:
         production_companies_cols = pickle.load(f)
-    return df, genres_cols, spoken_languages_cols, cast_cols, production_companies_cols
+    with open("cleaned_data/recommendation_movie_mappings.pickle", "rb") as f:
+        rec_movie_mappings = pickle.load(f)
+    movie_recs = load_npz("cleaned_data/movie_recommendations.npz")
+    return df, genres_cols, spoken_languages_cols, cast_cols, production_companies_cols, movie_recs, rec_movie_mappings
 
 
-df, genres_cols, spoken_languages_cols, cast_cols, production_companies_cols = get_data()
+df, genres_cols, spoken_languages_cols, cast_cols, production_companies_cols, movie_recs, rec_movie_mappings = get_data()
 
 
 def filter_df():
@@ -196,6 +201,9 @@ sidebar = st.sidebar
 with sidebar:
     st.markdown("""
             <style>
+            p {
+                text-align: justify;
+            }
             h1 {
                 padding-top: 10px;
             }
@@ -348,31 +356,100 @@ st.session_state.displayed_df
 st.write(f"## Movies By {st.session_state.sorting_method}")
 
 #! DON'T KNOW IF THE FOLLOWING WORKS FOR ANY VALUE OF MAX_ROWS AND MAX_COLUMNS !#
-MAX_ROWS = 4
-MAX_COLUMNS = 5
-total_filtered_movies = len(st.session_state.displayed_df)
-row_calc = total_filtered_movies / MAX_COLUMNS
-number_of_rows = min(MAX_ROWS, math.ceil(row_calc))
-# when not all the columns of the last row are filled,
-# the number of columns is calculated by the decimal part of the row_calc value multiplied by the max number of columns
-number_of_columns_in_last_row = math.ceil(
-    total_filtered_movies % MAX_COLUMNS) or MAX_COLUMNS
-columns_per_row = [MAX_COLUMNS] * \
-    (number_of_rows - 1) + [number_of_columns_in_last_row]
-poster_columns = [st.columns(MAX_COLUMNS)] * number_of_rows
-
-posters = [f"{idx}_w500.jpg" for idx in list(
-    st.session_state.displayed_df['tmdbId'])]
+# MAX_ROWS = 4
+# MAX_COLUMNS = 5
+# total_filtered_movies = len(st.session_state.displayed_df)
+# row_calc = total_filtered_movies / MAX_COLUMNS
+# number_of_rows = min(MAX_ROWS, math.ceil(row_calc))
+# number_of_columns_in_last_row = math.ceil(
+#    total_filtered_movies % MAX_COLUMNS) or MAX_COLUMNS
+# columns_per_row = [MAX_COLUMNS] * \
+#    (number_of_rows - 1) + [number_of_columns_in_last_row]
+# poster_columns = [st.columns(MAX_COLUMNS)] * number_of_rows
+#
+# posters = [f"{idx}_w500.jpg" for idx in list(
+#    st.session_state.displayed_df['tmdbId'])]
 titles = list(st.session_state.displayed_df['original_title'])
 
-for i, row_columns in enumerate(poster_columns):
-    for j, col in enumerate(row_columns[:columns_per_row[i]]):
-        try:
-            col.image("assets/posters/" +
-                      posters[i * MAX_COLUMNS + j], caption=f"{titles[i * MAX_COLUMNS + j]}")
-        except:
-            col.image("assets/posters/null_w500.jpg",
-                      caption=f"{titles[i * MAX_COLUMNS + j]}")
+html_posters = [f"""<td><a href='#' id='{idx}'><img width='93%' src='https://raw.githubusercontent.com/tiago-assis/Movie_Recommendation/main/assets/posters/{
+    idx}_w500.jpg'></a></td>""" for idx in list(st.session_state.displayed_df['tmdbId'])]
+
+MAX_COLUMNS = 5
+MAX_ROWS = 4
+start_display_index = 0
+end_display_index = MAX_ROWS * MAX_COLUMNS
+html_posters_to_display = "<table>"
+for i in range(start_display_index, end_display_index, MAX_COLUMNS):
+    html_posters_to_display += f"""<tr>{
+        "\n".join(html_posters[i:i+MAX_COLUMNS])}</tr><tr><td><p></p></td></tr>"""
+html_posters_to_display += "</table>"
+
+# for i, row_columns in enumerate(poster_columns):
+#    for j, col in enumerate(row_columns[:columns_per_row[i]]):
+#        try:
+#            col.image("assets/posters/" +
+#                      posters[i * MAX_COLUMNS + j], caption=f"{titles[i * MAX_COLUMNS + j]}")
+#        except:
+#            col.image("assets/posters/null_w500.jpg",
+#                      caption=f"{titles[i * MAX_COLUMNS + j]}")
 
 
+def get_recommendations(movie_id, knn=5):
+    idx = df.loc[df['tmdbId'] == movie_id, "movieId"].values[0]
+    mapped_idx = rec_movie_mappings[idx]
+    sims = movie_recs.getrow(mapped_idx).todense()
+    sims = sims.argsort().A1[:-(knn+2):-1][1:]
+    reverse_rec_movie_mappings = {v: k for k, v in rec_movie_mappings.items()}
+    sim_idxs = []
+    # TODO: PARALLELIZE THIS MAYBE
+    for idx in sims:
+        sim_idxs.append(reverse_rec_movie_mappings.get(idx))
+    return df.loc[df['movieId'].isin(sim_idxs), "tmdbId"]
+
+
+@st.dialog("Movie Details", width='large')
+def display_movie(movie_id):
+    movie_img, movie_info = st.columns([1.5, 2.5], gap='medium')
+    with movie_img:
+        st.image(f"assets/posters/{movie_id}_w500.jpg")
+    with movie_info:
+        filter = df['tmdbId'] == movie_id
+        title = df.loc[filter, 'original_title'].values[0]
+        year = df.loc[filter, 'release_date'].values[0]
+        rating = df.loc[filter, 'vote_weighted_average'].values[0]
+        overview = df.loc[filter, 'overview'].values[0]
+        genres = ast.literal_eval(df.loc[filter, 'genres'].values[0])
+        prod_companies = ast.literal_eval(
+            df.loc[filter, 'production_companies'].values[0])
+        imdb_page = f"https://www.imdb.com/title/{
+            df.loc[filter, 'imdb_id'].values[0]}"
+        homepage = df.loc[filter, 'homepage'].values[0]
+
+        movie_title, movie_rating = st.columns([0.8, 0.2])
+        with movie_title:
+            st.write(f"## {title} ({year})")
+        with movie_rating:
+            st.markdown(
+                f"""<h2 style='text-align:right'>{round(rating, 1)} ⭐</h2>""", unsafe_allow_html=True)
+        st.write(f"{overview}")
+        st.write(f"**Genres:** {', '.join(genres)}")
+        st.markdown(f"**Production Companies:** {', '.join(prod_companies)}")
+        st.markdown(f"""**More Info:** [IMDB Page]({imdb_page}){
+                    f', [Movie Homepage]({homepage})' if not isinstance(homepage, float) else ''}""")
+
+    st.write("**Movie Recomendations:**")
+    movie_rec_ids = get_recommendations(movie_id)
+    recs = st.columns(len(movie_rec_ids))
+    for i, id in enumerate(movie_rec_ids):
+        recs[i].image(f"assets/posters/{id}_w500.jpg")
+
+
+clicked = click_detector(html_posters_to_display)
+if clicked != "":
+    display_movie(int(clicked))
+
+
+# TODO: ABLE TO CLICK RECOMMENDED MOVIES
+# TODO: MORE ROBUST RECOMMENDATIONS
 # TODO: reset button can be spammed and errors occur due to missing session_state keys
+####!!!! TODO: ACRESCENTAR O RESTO DAS MATRIZES AO CACHE !!!!####
